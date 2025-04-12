70f8837160c068fbce28f01723a22989e8576e1e
Pratham Paleriya <prathampaleriya@gmail.com>
2025-04-12 18:29:17 +0530

    logger setup

--- backend/utils/constants.py
+++ backend/utils/constants.py
@@ -1,3 +1,26 @@
 EMBEDDING_MODEL = "text-embedding-3-small"
 OPENAI_MODEL = "gpt-4o-mini"
 REDIS_EXP = 3600 
+
+SKIP_FIELDS_LOGGER = (
+    "args",
+    "exc_info",
+    "exc_text",
+    "stack_info",
+    "msg",
+    "levelname",
+    "levelno",
+    "pathname",
+    "filename",
+    "module",
+    "created",
+    "msecs",
+    "relativeCreated",
+    "thread",
+    "threadName",
+    "processName",
+    "process",
+    "name",
+    "lineno",
+    "funcName",
+)
