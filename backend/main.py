2cc622508b6d45da8588709c1543353fd1357791
Pratham Paleriya <prathampaleriya@gmail.com>
2025-04-20 06:04:10 +0530

    vercel config

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import APIRouter, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -41,3 +42,5 @@ app.include_router(
 )
 
 
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
