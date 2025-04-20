26e3d80afa53eeb05ef628a14e6a43b09994ada5
Pratham Paleriya <prathampaleriya@gmail.com>
2025-04-20 14:14:42 +0530

    allow all origins

--- backend/main.py
+++ backend/main.py
@@ -15,10 +15,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://empathy.athams.com"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
