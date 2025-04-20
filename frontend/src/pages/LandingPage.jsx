6dfd861efcd0f63fb78b3223b282f0e8d2f46c23
Pratham Paleriya <prathampaleriya@gmail.com>
2025-04-21 03:21:53 +0530

    back to chat if token is present via landing

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -4,8 +4,20 @@ import Navbar from '../components/landing/NavBar';
 import About from '../components/landing/About';
 import Footer from '../components/landing/Footer';
 import Features from '../components/landing/Features';
+import useAuthAPI from '../libs/api_auth';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const LandingPage = () => {
+  const { isTokenValid } = useAuthAPI();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+      if (isTokenValid()) {
+        navigate('/chat');
+      }
+    }, []);
+
   return (
     <div className="min-h-screen bg-bg text-[#4f4f4f] font-manrope">
       <Navbar />
