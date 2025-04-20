8a09192116351141eb1ebeb371cd27170c30df88
Pratham Paleriya <prathampaleriya@gmail.com>
2025-04-20 20:59:57 +0530

    .

--- backend/main.py
+++ backend/main.py
@@ -15,10 +15,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://empathy.athams.com"],
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"], 
+    allow_headers=["*"], 
 )
 
 route = APIRouter()
